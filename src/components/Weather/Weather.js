6f684d17516f14dd7bf8b0d9c640f8f2ce65e7bd
ribeirolivia <ribeiro.livias@hotmail.com>
2022-01-27 22:24:19 -0300

    Add info

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,10 +1,11 @@
 import { React, useState } from "react";
-import { Card, Form, Button } from "react-bootstrap";
+import { Card, Form, Button, CardGroup } from "react-bootstrap";
 import '../../Style.css';
 
 const Weather = () => {
   const [city, setCity] = useState("");
   const [weatherForecast, setWeatherForecast] = useState(null);
+  const [forecast, setForecast] = useState(null)
 
   const handleChange = (e) => {
     setCity(e.target.value);
@@ -13,7 +14,7 @@ const Weather = () => {
 
   const handleSearch = () => {
     fetch(
-      `http://api.weatherapi.com/v1/current.json?key=e751ea9cf5bc4fcbbb3225428211810&q=${city}&lang=pt`
+      `http://api.weatherapi.com/v1/forecast.json?key=e751ea9cf5bc4fcbbb3225428211810&q=${city}&lang=pt` 
     )
       .then((response) => {
         if (response.status === 200) {
@@ -27,6 +28,24 @@ const Weather = () => {
       });
   };
 
+  const handleSearchForecast = () => {
+    fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=e751ea9cf5bc4fcbbb3225428211810&q=${city}&days=3&aqi=no&alerts=no&lang=pt`
+    )
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+        
+      })
+      .then((data) => {
+        setForecast(data);
+        console.log(data)
+      });
+      
+  };
+
+
   return (
     <div className='container pt-4 w-75'>
       <Card className='card'>
@@ -36,7 +55,7 @@ const Weather = () => {
           <Form.Control className='w-50' value={city} onChange={handleChange}></Form.Control>
           
           <Button  onClick={handleSearch} variant="primary">
-            Contar
+            Buscar
           </Button>
 
           
@@ -44,7 +63,7 @@ const Weather = () => {
 
         {
         weatherForecast ? (
-          <Card.Body>
+          <Card.Body >
             <Card.Title className="text-center">{weatherForecast.location.name}/{weatherForecast.location.region}</Card.Title>
             <div className='d-flex justify-content-center'>
             <img  alt={weatherForecast.current.condition.text} src={weatherForecast.current.condition.icon}/>
@@ -55,11 +74,26 @@ const Weather = () => {
             <Card.Text className="text-center">
                 Temperatura atual:{weatherForecast.current.temp_c}°C
             </Card.Text>
+            <Button  onClick={handleSearchForecast}>Previsão estendida</Button>
           </Card.Body>
         ) : null
         }
-
       </Card>
+
+        <div>
+      {
+        forecast ? (
+          <CardGroup>
+            <Card>
+              <Card.Body>
+                <Card.Text>{forecast.location.name}/{forecast.location.region}</Card.Text>
+              {/* <Card.Title>{forecast.forecastday}</Card.Title> */}
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        ) : null
+      }
+        </div> 
     </div>
   );
 };
